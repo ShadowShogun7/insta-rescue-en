f1b17f28ec57228cb1f4c2ddc63bf29ce9a85aef
Nova OS <tysonchenys@gmail.com>
2026-04-15 00:15:15 +0800

    feat: move recovery guide to dedicated us path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AdminLogin from "./pages/admin/Login";
@@ -18,7 +19,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter basename="/us">
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/instagram-recovery-guide" element={<Index />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route
             path="/admin/live-chat"
